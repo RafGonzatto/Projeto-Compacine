0ea2dd45a7d79054fb5a000b5fdaa762155337a9
Bernardo Batistelli <bebatistelli254@gmail.com>
2024-06-05 16:08:22 -0300

    feat : criar repositorios do ticket

--- src/models/ticket.model.ts
+++ src/models/ticket.model.ts
@@ -1,43 +1,42 @@
 import {
-    Entity,
-    Column,
-    BaseEntity,
-    ManyToOne,
-    PrimaryGeneratedColumn,
-    BeforeInsert,
-    BeforeUpdate,
-    Unique,
-  } from 'typeorm';
-  //import { Session } from './Session';
-  import { ITicket } from '../interfaces/ticket.interface';
-  @Entity('Ticket')
-  @Unique(['session', 'chair']) 
-  export class Ticket extends BaseEntity  implements ITicket  {
-    @PrimaryGeneratedColumn()
-    id!: number;
-  
-    @Column()
-    session_id!: number;
-  
-    @Column()
-    chair!: string;
-  
-    @Column()
-    value!: number;
-  
-    // @ManyToOne(() => Session, session => session.tickets)
-    // session: Session;
+  Entity,
+  Column,
+  BaseEntity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  BeforeInsert,
+  BeforeUpdate,
+  Unique,
+} from 'typeorm'
+//import { Session } from './Session';
+import { ITicket } from '../interfaces/ticket.interface'
+@Entity('Ticket')
+@Unique(['session', 'chair'])
+export class Ticket extends BaseEntity implements ITicket {
+  @PrimaryGeneratedColumn()
+  id!: number
 
-   // @BeforeInsert()
-   // @BeforeUpdate()
-//     async checkUniqueChairPerSession() {
-//       const ticket = await Ticket.findOne({
-//         where: { session: this.session, chair: this.chair },
-//       });
-  
-//       if (ticket) {
-//         throw new Error('Chair already taken in this session');
-//       }
-//     }
-   }
-  
+  @Column()
+  session_id!: number
+
+  @Column()
+  chair!: string
+
+  @Column()
+  value!: number
+
+  // @ManyToOne(() => Session, session => session.tickets)
+  // session: Session;
+
+  // @BeforeInsert()
+  // @BeforeUpdate()
+  //     async checkUniqueChairPerSession() {
+  //       const ticket = await Ticket.findOne({
+  //         where: { session: this.session, chair: this.chair },
+  //       });
+
+  //       if (ticket) {
+  //         throw new Error('Chair already taken in this session');
+  //       }
+  //     }
+}
