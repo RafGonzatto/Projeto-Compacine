892c8ed05e72b3ee9ed7f280c8f57057503fd7f6
Nathan Weirich <nathanweirich@gmail.com>
2024-06-06 15:16:07 -0300

    feat(session): add session to container

--- src/container.ts
+++ src/container.ts
@@ -1,8 +1,9 @@
 import { container } from 'tsyringe'
 import IMovieRepository from './repositories.interfaces/movie.repository.interface'
 import MovieRepository from './repositories/movie.repository'
+import ISessionRepository from 'repositories.interfaces/session.repository.interface'
+import { SessionRepository } from 'repositories/session.repository'
 
-container.registerSingleton<IMovieRepository>(
-  'MovieRepository',
-  MovieRepository,
-)
+container.registerSingleton<IMovieRepository>('MovieRepository', MovieRepository)
+
+container.registerSingleton<ISessionRepository>('SessionRepository', SessionRepository)
